0ce95d494c3dd0d27b592917ddceb931a1c18f8e
Sumesh K V <sumesh@i-serve.com.my>
2021-07-28 11:44:33 +0800

    developer component

--- resources/js/components/Developer.vue
+++ resources/js/components/Developer.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-md-8">
+      <div class="col-md-12">
         <div class="card">
-          <div class="card-header">Example Component</div>
+          <div class="card-header">Developer List</div>
 
           <div class="card-body">
             I'm an example component.
@@ -17,7 +17,7 @@
 <script>
 export default {
   mounted() {
-    console.log('Component mounted.')
+    console.log('Developer Component Loaded!!!')
   },
 }
 </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,32 +49,29 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            <strong>{{ Auth::user()->name }}</strong>
+                            <a href="{{ route('logout') }}" class="ml-3"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4" id="app">
             @yield('content')
         </main>
     </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    @yield('script')
 </body>
 </html>
